e928db0916a161a916a57ee8178a2db4628cb9c2
Josiah Berkebile <jgberkebile@dstsystems.com>
2016-04-28 15:48:38 -0500

    fix gitignore

--- haskell_monoids/Magma.hs
+++ haskell_monoids/Magma.hs
@@ -3,14 +3,18 @@ module Magma (
   , Sum(..)
 ) where
 
+-- First, declare what a Magma is
 class Magma a where
   (<>) :: a -> a -> a
 
+-- Now, make the Sum type, and have it be 'Show'able and 'Eq'uatable.
 newtype Sum a = Sum a deriving (Show, Eq)
 
+-- Then, declare that Sum is a Num so that we can use the + op on it generically.
 instance Num a => Num (Sum a) where
   (+) (Sum x) (Sum y) = Sum (x + y)
   fromInteger = Sum . fromInteger
 
+-- Now, declare that Sum is a Magma
 instance Num a => Magma (Sum a) where
   (<>) = (+)

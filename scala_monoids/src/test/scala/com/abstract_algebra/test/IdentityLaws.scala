4c814b1356b5567423c9f9e92b58cd9dda2400ec
Josiah Berkebile <jgberkebile@dstsystems.com>
2016-04-28 15:47:35 -0500

    improve identity tests

--- scala_monoids/src/test/scala/com/abstract_algebra/test/IdentityLaws.scala
+++ scala_monoids/src/test/scala/com/abstract_algebra/test/IdentityLaws.scala
@@ -1,19 +1,19 @@
 package com.abstract_algebra.test
 
-import com.abstract_algebra.SumIdentity
+import com.abstract_algebra.{SumIdentity, SumRightIdentity, SumLeftIdentity}
 
 import org.scalacheck.{Arbitrary, Prop}
 
 object IdentityLaws {
   def rightIdentity[A: Numeric: Arbitrary]: Prop = Prop.forAll { (x: A) =>
-    val rightId = SumIdentity[A]
+    val rightId = SumRightIdentity[A]
     import rightId.{rightEmpty, concat}
 
     concat(x, rightEmpty) == x
   }
 
   def leftIdentity[A: Numeric: Arbitrary]: Prop = Prop.forAll { (x: A) =>
-    val leftId = SumIdentity[A]
+    val leftId = SumLeftIdentity[A]
     import leftId.{leftEmpty, concat}
 
     concat(leftEmpty, x) == x
